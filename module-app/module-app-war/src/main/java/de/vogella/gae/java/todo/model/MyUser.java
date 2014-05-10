cd2c303204a5c9d39fa8e5475afaf002b4e4acfd
Vladimir Sosnin <antiso@gmail.com>
2014-05-10 19:21:28 +0400

    ToDo application ported

---
+++ module-app/module-app-war/src/main/java/de/vogella/gae/java/todo/model/MyUser.java
@@ -0,0 +1,39 @@
+package de.vogella.gae.java.todo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MyUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private String id;
+	private String userId;
+	private String email;
+
+	public MyUser() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public MyUser(String userId, String email) {
+		this.id = userId;
+		this.userId = userId;
+		this.email = email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+}
